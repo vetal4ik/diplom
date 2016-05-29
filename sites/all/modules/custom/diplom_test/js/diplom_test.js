255658b280a484fa21fe71c32c90f8d985ba2478
vetal4ik <naslednik10@gmail.com>
2016-05-29 18:24:10 +0300

    Custom

--- sites/all/modules/custom/diplom_test/js/diplom_test.js
+++ sites/all/modules/custom/diplom_test/js/diplom_test.js
@@ -1,3 +1,18 @@
-/**
- * Created by vetal4ik on 28.05.16.
- */
+(function ($) {
+
+// Store our function as a property of Drupal.behaviors.
+    Drupal.behaviors.testTimer = {
+        attach: function (context, settings) {
+            var seconds = $('.test-timer .value').text();
+            var form = $('.test-timer .value').parents('form');
+            $('.test-timer .value').countdown({
+                until: seconds,
+                layout: '{hn}:{mn}:{sn}',
+                onExpiry: function () {
+                    $('input[name="finish"]', form).click();
+                }
+            });
+        }
+    }
+
+}(jQuery));
